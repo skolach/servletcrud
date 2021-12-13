7ec2d9807ebdb2988d43c1a69880cdf98a9ad0ac
skolach <ssskolach@gmail.com>
2021-12-13 17:24:59 +0200

    redesign for singleton connection

--- src/main/java/com/globallogic/edu/dao/OrderDao.java
+++ src/main/java/com/globallogic/edu/dao/OrderDao.java
@@ -1,10 +1,12 @@
 package com.globallogic.edu.dao;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,39 +37,6 @@ public class OrderDao {
             "from `order` o " +
             "where o.id = ? ";
 
-    public static void insertOrder(Order order) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = DbManager.getInstance().getConnection();
-            pstmt = con.prepareStatement(SQL_INSERT_ORDER, Statement.RETURN_GENERATED_KEYS);// NOSONAR
-            int k = 1;
-            pstmt.setInt(k++, order.getUserId());
-            pstmt.setTimestamp(k++, order.getCreatedAt());
-            pstmt.setTimestamp(k++, order.getStartAt());
-            pstmt.setTimestamp(k++, order.getEndAt());
-            pstmt.setBigDecimal(k++, order.getPrice());
-            pstmt.setInt(k++, order.getRouteDiscount());
-            pstmt.setInt(k++, order.getUserDiscount());
-            pstmt.setBigDecimal(k, order.getCash());
-
-            if (pstmt.executeUpdate() > 0) {
-                ResultSet rs = pstmt.getGeneratedKeys();
-                if (rs.next()) {
-                    order.setId(rs.getInt(1));
-                }
-                rs.close();
-            }
-            pstmt.close();
-        } catch (SQLException ex) {
-            DbManager.getInstance().rollbackAndClose(con);
-            log.error("Cannot insert order to DB", ex);
-            throw new SQLException(ex);
-        } finally {
-            DbManager.getInstance().commitAndClose(con);
-        }
-    }
-
     public static Order getOrder(String id) throws SQLException {
         Order order = new Order();
         PreparedStatement pstmt = null;
@@ -75,7 +44,7 @@ public class OrderDao {
         Connection con = null;
 
         try {
-            con = DbManager.getInstance().getConnection();
+            con = DbManager.getConnection();
             pstmt = con.prepareStatement(SQL_SELECT_ORDER); // NOSONAR
             pstmt.setInt(1, Integer.parseInt(id));
             rs = pstmt.executeQuery();
@@ -95,25 +64,19 @@ public class OrderDao {
             rs.close();
             pstmt.close();
         } catch (SQLException | NumberFormatException ex) {
-            DbManager.getInstance().rollbackAndClose(con);
             log.error("Cannot get order by id = " + id, ex);
             throw new SQLException(ex);
-        } finally {
-            DbManager.getInstance().commitAndClose(con);
         }
         return order;
     }
 
     public static List<Order> getOrders() throws SQLException {
-        List<Order> result = new ArrayList<>();
         Statement stmt = null;
-        ResultSet rs = null;
-        Connection con = null;
-
+        List<Order> result = new ArrayList<>();
         try {
-            con = DbManager.getInstance().getConnection();
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(SQL_SELECT_ORDERS);
+            Connection con = DbManager.getConnection();
+            stmt = con.createStatement(); //NOSONAR
+            ResultSet rs = stmt.executeQuery(SQL_SELECT_ORDERS);
             while (rs.next()) {
                 Order order = new Order();
                 order.setId(rs.getInt("id"));
@@ -128,22 +91,54 @@ public class OrderDao {
 
                 result.add(order);
             }
-            rs.close();
             stmt.close();
         } catch (SQLException ex) {
-            DbManager.getInstance().rollbackAndClose(con);
             log.error("Cannot get list of orders", ex);
             throw new SQLException(ex);
-        } finally {
-            DbManager.getInstance().commitAndClose(con);
         }
         return result;
     }
 
-    public static void main(String[] args) throws SQLException {
-        for (Order o : getOrders()) {
-            System.out.println(o);
+    public static void insertOrder(Order order) throws SQLException {
+        try {
+            Connection con = DbManager.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(SQL_INSERT_ORDER); //NOSONAR
+                //(`user_id`, `start_at`, `end_at`, `price`, `route_discount`, `user_discount`, `cash`)
+            int k = 0;
+            pstmt.setInt(++k, 1);
+            pstmt.setTimestamp(++k, order.getStartAt());
+            pstmt.setTimestamp(++k, order.getEndAt());
+            pstmt.setBigDecimal(++k, order.getPrice());
+            pstmt.setInt(++k, order.getRouteDiscount());
+            pstmt.setInt(++k, order.getUserDiscount());
+            pstmt.setBigDecimal(++k, order.getCash());
+            pstmt.execute();
+        } catch (SQLException e) {
+            log.error("Can't insert new order to the DB", e);
         }
     }
 
+    public static void main(String[] args) throws SQLException {
+
+        for (Order o : getOrders()) {
+            System.out.println(o.getStartAt() + " " + o.getUserId());
+        }
+
+        Order order = new Order();
+        //(`user_id`, `start_at`, `end_at`, `price`, `route_discount`, `user_discount`, `cash`)
+        order.setUserId(999);
+        order.setStartAt(Timestamp.valueOf("1010-10-10 10:10:10"));
+        order.setEndAt(Timestamp.valueOf("1010-10-10 10:10:10"));        
+        order.setPrice(new BigDecimal("10.10"));
+        order.setRouteDiscount(10);
+        order.setUserDiscount(10);
+        order.setCash(new BigDecimal("10.10"));
+        insertOrder(order);
+
+        System.out.println("----------");
+
+        for (Order o : getOrders()) {
+            System.out.println(o.getStartAt() + " " + o.getUserId());
+        }
+    }
 }
