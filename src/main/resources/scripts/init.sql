e24043414a38ebecaef6c051859b1b1c124ff0bc
skolach <ssskolach@gmail.com>
2021-12-24 15:10:52 +0200

    DB redesign for one-to-many relation between `order_` and `route`

--- src/main/resources/scripts/init.sql
+++ src/main/resources/scripts/init.sql
@@ -12,16 +12,11 @@ INSERT into `order_`(
         `user_discount`,
         `cash`
     )
-VALUES (1, now(), now(), now(), 100, 10, 20, 72);
+VALUES
+(1, now(), now(), now(), 100, 10, 20, 72),
+(2, now(), now(), now(), 40, 5, 6, 30);
 
-INSERT into `order_`(
-        `user_id`,
-        `created_at`,
-        `start_at`,
-        `end_at`,
-        `price`,
-        `route_discount`,
-        `user_discount`,
-        `cash`
-    )
-VALUES (2, now(), now(), now(), 40, 5, 6, 30);
+insert into `route`(`order_id`, `point_order`, `point_name`)
+values
+	(1,1,'1_1'), (1,2,'1_2'), (1,3,'1_3'),
+    (2,1,'2_1'), (2,2,'2_2'), (2,3,'2_3');
