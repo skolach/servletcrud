93328ae4c3aee19ebe134d7194676254a6ed743a
skolach <ssskolach@gmail.com>
2022-01-03 13:10:41 +0200

    ...

--- src/main/java/com/globallogic/edu/controller/OrderController.java
+++ src/main/java/com/globallogic/edu/controller/OrderController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = {"/","/order"})
+@RequestMapping(path = {"/order"})
 public class OrderController {
 
     @Autowired
