952db3fb2ef8c65637a2539f01c749cd663cb659
skolach <skolach@polynet.lviv.ua>
2022-01-11 23:14:36 +0200

    removed redirect "/" -> "/order"

--- src/main/java/com/globallogic/edu/configuration/MvcConfig.java
+++ src/main/java/com/globallogic/edu/configuration/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.globallogic.edu.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +14,6 @@ public class MvcConfig implements WebMvcConfigurer{
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addRedirectViewController("/", "/order");
     }
 
 }
