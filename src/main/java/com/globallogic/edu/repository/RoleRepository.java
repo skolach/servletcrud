efcbe4b92e01d364a68fdd814710e44d75c72148
skolach <ssskolach@gmail.com>
2022-01-13 14:58:44 +0200

    RoleRepository.java

---
+++ src/main/java/com/globallogic/edu/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.globallogic.edu.repository;
+
+import com.globallogic.edu.entity.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+    
+}
